464ba437853169901c6c9553728111ad379b3fe1
Michael Hacker <hack1973@swbell.net>
2020-10-18 21:36:54 -0500

    completed array practice

---
+++ ArrayPractice/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ArrayPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 1, 2, 3, 5, 8 };
+
+            foreach (int num in nums)
+            {
+                if (num % 2 != 0)
+                {
+                    Console.WriteLine(num);
+                }
+            }
+        }
+    }
+}
